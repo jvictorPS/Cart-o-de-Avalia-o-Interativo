b278a9242a2e0fbb5c38fde2c98c3257861e1617
João Victor <j.victorbahia@gmail.com>
2023-06-11 00:02:35 -0300

    logica para não enviar nota vazia

--- main.js
+++ main.js
@@ -5,27 +5,37 @@ const cartaoAgradecimento = document.querySelector('#cartao-agradecimento')
 
 let nota = ""
 
-notas.forEach ( (elemento) => {
-    elemento.addEventListener('click', (evento) => {
-        
-        avaliar(evento.target.textContent)
-        
-    })
-})
-
-botaoAvaliar.addEventListener('click' , () => {
-
-    cartaoAvaliacao.style.display = "none"
-    cartaoAgradecimento.style.display = "flex"
-    cartaoAgradecimento.style.flexDirection = "column"
-    cartaoAgradecimento.style.justifyContent = "center"
-    cartaoAgradecimento.style.alignItems = "center"
-
-})
-
 function avaliar (nota) {
     let notaDada = `You selected out ${nota} of 5`
 
     const tagAvaliacaoFeita = document.querySelector('#avaliacao-feita')
     tagAvaliacaoFeita.textContent = notaDada
 }
+
+notas.forEach ( (elemento) => {
+    elemento.addEventListener('click', (evento) => {
+        
+        nota = evento.target.textContent
+
+        avaliar(nota)
+        
+    })
+})
+
+function enviar () {
+
+    if(nota === "") {
+        alert('Escolha uma nota!')
+        return
+    }
+
+    cartaoAvaliacao.style.display = "none"
+    cartaoAgradecimento.style.display = "flex"
+    cartaoAgradecimento.style.flexDirection = "column"
+    cartaoAgradecimento.style.justifyContent = "center"
+    cartaoAgradecimento.style.alignItems = "center"
+}
+
+botaoAvaliar.addEventListener('click' , () => {
+    enviar()
+})
